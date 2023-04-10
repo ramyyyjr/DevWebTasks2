6af9e112ee5ef998aa0a271514dd4b7d11bbf359
ramyyyjr <117462665+ramyyyjr@users.noreply.github.com>
2023-04-10 20:24:07 +0100

    Add files via upload

---
+++ Creative Web/index.js
@@ -0,0 +1,7 @@
+// const themeToggleBtn = document.querySelector('#theme-toggle');
+// const body = document.querySelector('body');
+
+// themeToggleBtn.addEventListener('click', () => {
+//     body.classList.toggle('light-mode');
+// });
+// changing theme:
